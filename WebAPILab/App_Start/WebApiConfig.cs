91b8dece778f86e344a7fb8b58988091a7772430
Pierce C Lauring <pclauring@gmail.com>
2017-12-01 09:01:53 -0500

    First Commit

--- WebAPILab/App_Start/WebApiConfig.cs
+++ WebAPILab/App_Start/WebApiConfig.cs
@@ -14,9 +14,13 @@ namespace WebAPILab
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ WebAPILab/Controllers/MovieDBController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPILab.Models;
+
+namespace WebAPILab.Controllers
+{
+    public class MovieDBController : ApiController
+    {
+
+        [HttpGet]
+        public List<Movy> GetMovies()
+        {
+            MovieAppDBEntities ORM = new MovieAppDBEntities();
+
+            return ORM.Movies.ToList();
+        }
+
+        [HttpGet]
+        public List<Movy> GetMoviesByCategory(string Category)
+        {
+            MovieAppDBEntities ORM = new MovieAppDBEntities();
+
+            return ORM.Movies.Where(x => x.Category == Category).ToList();
+        }
+
+        [HttpGet]
+        public Movy GetRandomMovie()
+        {
+            MovieAppDBEntities ORM = new MovieAppDBEntities();
+
+            Random random = new Random();
+
+            List<Movy> movieList = ORM.Movies.ToList();
+
+            Movy Choice = movieList[random.Next(movieList.Count)];
+
+            return Choice;
+        }
+
+        [HttpGet]
+        public Movy GetRandomMoviesFromCategory(string Category)
+        {
+            MovieAppDBEntities ORM = new MovieAppDBEntities();
+
+            Random random = new Random();
+
+            List<Movy> movieList = ORM.Movies.Where(x => x.Category == Category).ToList();
+
+            Movy Choice = movieList[random.Next(movieList.Count)];
+
+            return Choice;
+
+        }
+
+        [HttpGet]
+        public List<Movy> GetRandomMovieList(int Length)
+        {
+            MovieAppDBEntities ORM = new MovieAppDBEntities();
+
+            Random random = new Random();
+
+            List<Movy> movieList = ORM.Movies.ToList();
+            List<Movy> randomList = new List<Movy>();
+            int index = 0;
+            while (Length > index)
+            {
+                randomList.Add(movieList[random.Next(movieList.Count)]);
+                index++;
+            }
+            return randomList;
+
+        }
+    }
+}
